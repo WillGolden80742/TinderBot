01eb44aefe738e6c55764aacedcf73e69943e1a9
WillGolden80742 <91426752+WillGolden80742@users.noreply.github.com>
2024-01-03 23:56:47 -0300

    Update TinderBot.js

--- TinderBot.js
+++ TinderBot.js
@@ -2,10 +2,13 @@ function getRandomBoolean() {
   return Math.random() < 0.5;
 }
 
+function getButtonByBackgroundClass(backgroundClass) {
+  return Array.from(document.querySelectorAll('.button')).find(element => element.classList.contains(backgroundClass));
+}
+
 function clickRandomButton() {
-  const hiddenSpans = document.querySelectorAll('span.Hidden');
-  const likeButton = Array.from(hiddenSpans).find(span => span.innerHTML.trim() === 'Curti').parentNode.parentNode.parentNode;
-  const dislikeButton = Array.from(hiddenSpans).find(span => span.innerHTML.trim() === 'Não').parentNode.parentNode.parentNode;
+  const likeButton = getButtonByBackgroundClass('Bgi($g-ds-background-like):a');
+  const dislikeButton = getButtonByBackgroundClass('Bgi($g-ds-background-nope):a');
 
   if (!likeButton || !dislikeButton) {
     console.error("Botões não encontrados. Certifique-se de que está na página certa.");
@@ -19,7 +22,7 @@ function clickRandomButton() {
 
 function executeWithRandomInterval() {
   clickRandomButton();
-  const randomInterval = Math.floor(Math.random() * (1500 - 500 + 1)) + 500;
+  const randomInterval = Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
   setTimeout(executeWithRandomInterval, randomInterval);
 }
 
